cda25d828e49bc00fc883d46f319474fd53da746
Noam <noams@epd-medical.com>
2020-10-04 21:01:23 +0300

    added ranges for data loader

--- defect_segmentation/data_loading/DatasetSingleImage.py
+++ defect_segmentation/data_loading/DatasetSingleImage.py
@@ -5,11 +5,20 @@ import os
 
 
 class DatasetSingleImage(Dataset):
-    def __init__(self, image_path):
+    def __init__(self, image_path: str, sample_shape: tuple, strides: tuple):
         self._path = image_path
         assert os.path.isfile(self._path)
         self._im = cv2.imread(self._path)
         self._shape = self._im.shape
+        self._rows, self._cols = self._shape[0], self._shape[1]
+
+        self._sample_shape = sample_shape
+        self._sample_rows, self._sample_cols = self._sample_shape[0], self._sample_shape[1]
+
+        self._strides = strides
+        self._stride_rows, self._stride_cols = self._strides[0], self._strides[1]
+        self._rows_start_range = range(0, self._rows, self._stride_rows)
+        self._cols_start_range = range(0, self._cols, self._stride_cols)
 
     def __len__(self):
         return 1
@@ -17,10 +26,16 @@ class DatasetSingleImage(Dataset):
     def __getitem__(self, item):
         return self._im
 
+
 if __name__ == "__main__":
-    path = ConfigProvider.config().data.defective_inspected_path1
-    dataset = DatasetSingleImage(path)
-    for i in range(len(dataset)):
-        sample = dataset[i]
-        cv2.imshow(f"sample #{i}", sample)
-        cv2.waitKey(0)
+    def main():
+        path = ConfigProvider.config().data.defective_inspected_path1
+        sample_shape = (15, 15)
+        strides = (1, 1)
+        dataset = DatasetSingleImage(path, sample_shape, strides)
+        for i in range(len(dataset)):
+            sample = dataset[i]
+            cv2.imshow(f"sample #{i}", sample)
+            cv2.waitKey(0)
+
+    main()
