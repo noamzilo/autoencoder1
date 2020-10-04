c69e5284a1d44507693af74c9afea07637e8803e
Noam <noams@epd-medical.com>
2020-10-04 22:44:45 +0300

    fixed end of image problems with strides

--- defect_segmentation/data_loading/DatasetSingleImage.py
+++ defect_segmentation/data_loading/DatasetSingleImage.py
@@ -21,9 +21,9 @@ class DatasetSingleImage(Dataset):
         # self._cols_start_range = range(0, self._cols, self._stride_cols)
 
         self._rows_tuples_range = \
-            [(c, min(c + self._sample_rows, self._rows)) for c in range(0, self._rows, self._stride_rows)]
+            [(c, min(c + self._sample_rows, self._rows)) for c in range(0, self._rows - self._sample_rows, self._stride_rows)]
         self._cols_tuples_range = \
-            [(r, min(r + self._sample_cols, self._cols)) for r in range(0, self._cols, self._stride_cols)]
+            [(r, min(r + self._sample_cols, self._cols)) for r in range(0, self._cols - self._sample_cols, self._stride_cols)]
 
         self._n_strides_rows = len(self._rows_tuples_range)
         self._n_strides_cols = len(self._cols_tuples_range)
@@ -37,7 +37,9 @@ class DatasetSingleImage(Dataset):
         col_ind = ind % self._n_strides_cols
         sample_x = self._rows_tuples_range[row_ind]
         sample_y = self._cols_tuples_range[col_ind]
-        return self._im[sample_x[0]:sample_x[1], sample_y[0]:sample_y[1]]
+        sample = self._im[sample_x[0]:sample_x[1], sample_y[0]:sample_y[1]]
+        assert sample.shape[:2] == self._sample_shape
+        return sample
 
 
 if __name__ == "__main__":
